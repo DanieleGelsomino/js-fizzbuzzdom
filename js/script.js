d61448ca435ef1a265955e0bd9bf3a52bbbc7e14
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-03-08 16:44:58 +0100

    add milestone 3 complete

--- js/script.js
+++ js/script.js
@@ -13,29 +13,30 @@ MILESTONE 3
 Applica uno stile differente a seconda del valore dell'indice per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5.
 */
 
-//MILESTONE 1 + MILESTONE 2
+//MILESTONE 1 + MILESTONE 2 + MILESTONE 3
 
 const containerNumber = document.querySelector(".container");
 for(let number=1; number <=100; number++){
 
     if (number % 3 === 0 && number % 5 === 0) {
         console.log('FizzBuzz');
-        const element = `<div class="square-numbers">FizzBuzz</div>`;
+        let element = `<div id="tomato" class="square-numbers">FizzBuzz</div>`;
         containerNumber.innerHTML += element;
-
+    
     }else if (number % 5 === 0) {
         console.log('Buzz');
-        const element = `<div class="square-numbers">Buzz</div>`;
+        const element = `<div id="green" class="square-numbers">Buzz</div>`;
         containerNumber.innerHTML += element;
 
     }else if (number % 3 === 0){
-            console.log('Fizz');
-            const element = `<div class="square-numbers">Fizz</div>`;
-            containerNumber.innerHTML += element;
+        console.log('Fizz');
+        const element = `<div id="lilac" class="square-numbers">Fizz</div>`;
+        containerNumber.innerHTML += element;
+
     }else {
-         console.log(number);
-         const element = `<div id="square-${number}" class="square-numbers">${number}</div>`;
-         containerNumber.innerHTML += element;
+        console.log(number);
+        const element = `<div class="square-numbers">${number}</div>`;
+        containerNumber.innerHTML += element;
     }
 
 }
